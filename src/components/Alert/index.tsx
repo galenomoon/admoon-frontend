056fbe1f91ccaa202ee2e0de2d48eab922874cef
galenomoon <galeno.dev@gmail.com>
2023-09-20 22:22:10 -0300

    alert responsiveness

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,45 +1,51 @@
-import React from 'react'
+import React from "react"
 
 interface AlertProps {
-  onConfirm: () => void,
-  close: () => void,
-  message: string,
-  warning?: string,
-  title: string,
-  isOpen: boolean,
+  onConfirm: () => void
+  close: () => void
+  message: string
+  warning?: string
+  title: string
+  isOpen: boolean
 }
 
-export default function Alert({ onConfirm, close, message, warning, isOpen, title }: AlertProps) {
-  return !isOpen ? null :
-    <div className='animate-fade-in w-screen fixed left-0 top-0 z-[901] backdrop-blur-md h-screen overflow-hidden max-w-screen max-h-screen flex items-center justify-center bg-black bg-opacity-20 transition-all ease-out'>
-      <div className='bg-white max-w-[30vw] animate-slide-in rounded-xl shadow-lg items-center flex flex-col gap-8 p-8'>
-        <div className='flex flex-col items-center'>
-          <h1 className='text-xl font-satoshi-medium text-center'>
-            {title}
-          </h1>
-          <p className='text-lg mt-3 font-satoshi-regular text-center opacity-80'>
+export default function Alert({
+  onConfirm,
+  close,
+  message,
+  warning,
+  isOpen,
+  title,
+}: AlertProps) {
+  return !isOpen ? null : (
+    <div className="animate-fade-in max-w-screen fixed left-0 top-0 z-[901] flex h-screen max-h-screen w-screen items-center justify-center overflow-hidden bg-black bg-opacity-20 backdrop-blur-md transition-all ease-out">
+      <div className="animate-slide-in flex md:max-w-[30vw] flex-col items-center gap-8 rounded-xl bg-white p-8 shadow-lg">
+        <div className="flex flex-col items-center">
+          <h1 className="font-satoshi-medium text-center text-xl">{title}</h1>
+          <p className="font-satoshi-regular mt-3 text-center text-lg opacity-80">
             {message}
           </p>
-          {!warning ? null :
-            <p className='text-md text-red-600 font-satoshi-regular text-center opacity-80'>
+          {!warning ? null : (
+            <p className="text-md font-satoshi-regular text-center text-red-600 opacity-80">
               {warning}
             </p>
-          }
+          )}
         </div>
-        <div className='flex gap-4'>
+        <div className="flex gap-4">
           <button
             onClick={onConfirm}
-            className='bg-red-600 hover:opacity-80 duration-200 text-white w-[128px] py-2 rounded-lg font-satoshi-medium'
+            className="font-satoshi-medium w-[128px] rounded-lg bg-red-600 py-2 text-white duration-200 hover:opacity-80"
           >
             Confirmar
           </button>
           <button
             onClick={close}
-            className='bg-gray-200 hover:opacity-80 duration-200 w-[128px] py-2 rounded-lg font-satoshi-medium'
+            className="font-satoshi-medium w-[128px] rounded-lg bg-gray-200 py-2 duration-200 hover:opacity-80"
           >
             Cancelar
           </button>
         </div>
       </div>
     </div>
+  )
 }
