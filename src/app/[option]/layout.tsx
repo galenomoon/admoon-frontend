99c2de9c41fe18270627129b1180a6f3b058a61a
galenomoon <galeno.dev@gmail.com>
2023-11-02 23:52:28 -0300

    server side layout

--- src/app/[option]/layout.tsx
+++ src/app/[option]/layout.tsx
@@ -1,41 +1,45 @@
-
+"use server"
 //next
-import { Metadata } from "next";
+import { Metadata } from "next"
 
 //components
-import Menu, { TabNavigation } from "@/components/Menu";
+import Menu, { TabNavigation } from "@/components/Menu"
 
 //styles
-import { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast"
 
-export async function generateMetadata({ params: { option } }: { params: { option: string } }): Promise<Metadata> {
+export async function generateMetadata({
+  params: { option },
+}: {
+  params: { option: string }
+}): Promise<Metadata> {
   function slugParser(slug: string) {
-    const separated = slug.split("-");
-    const capitalized = separated.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
-    const text = capitalized.join(" ");
-    return text;
+    const separated = slug.split("-")
+    const capitalized = separated.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1),
+    )
+    const text = capitalized.join(" ")
+    return text
   }
   return {
     title: `${slugParser(option)} | Admoon`,
     description: "Admoon - The best way to manage your website",
-  };
+  }
 }
 
-
-export default function AdminSideLayout({
+export default async function AdminSideLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-
   return (
-    <main className="relative flex md:flex-row sm:flex-col min-h-screen w-fit font-satoshi-regular sm:bg-white md:bg-[#eee]">
+    <main className="font-satoshi-regular relative flex min-h-screen w-fit sm:flex-col sm:bg-white md:flex-row md:bg-[#eee]">
       <Menu />
-      <section className="flex flex-col sm:px-0 md:px-12 pt-12 md:w-[calc(100vw-324px)]">
+      <section className="flex flex-col pt-12 sm:px-0 md:w-[calc(100vw-324px)] md:px-12">
         {children}
       </section>
       <TabNavigation />
       <Toaster position="top-right" />
     </main>
-  );
+  )
 }
